e028794696c75208e35f3c89f98ca5789bbc43b5
Sai-tej18 <saitejuga@gmail.com>
2025-07-05 12:04:20 +0530

    Add files via upload

---
+++ training/day1.js
@@ -0,0 +1,17 @@
+console.log("hello")
+alert("hello")
+
+//DATA TYPES
+var A=10
+let B=20
+const C=30
+
+//STRING
+let name='balaya'
+let movie="legend"
+alart([name,movie])
+
+//NUMBER
+let p=20 //int
+let q=1.5 //float
+console.log({'INTEGER':P 'FLOAT':G})
